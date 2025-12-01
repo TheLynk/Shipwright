15b75a768e033d7fd9f4e0c47913adcbc6a614c4
TheLynk <jeanpol4226@gmail.com>
2025-12-01 03:16:25 +0100

    Add branch "Add Edit buttons for custom presets" | V6.0

--- soh/soh/Enhancements/gameplaystats.cpp
+++ soh/soh/Enhancements/gameplaystats.cpp
@@ -446,7 +446,7 @@ void DrawGameplayStatsHeader() {
     // if tag is empty (not a release build)
     if (gGitCommitTag[0] == 0) {
         GameplayStatsRow("Git Branch:", (char*)gGitBranch);
-        GameplayStatsRow("Version : 5.0 | Git Commit Hash:", (char*)gGitCommitHash);
+        GameplayStatsRow("Version : 6.0 | Git Commit Hash:", (char*)gGitCommitHash);
     } else {
         GameplayStatsRow("Build Version:", (char*)gBuildVersion);
     }
